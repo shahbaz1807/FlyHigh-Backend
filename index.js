f10c4022c1b76102abe0fd2ca88a381cac0aac1f
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-04-02 05:56:36 +0500

    notes api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import connectDB from './config/db.js';
-// import notesRoute from './routes/notesRoute.js';
-import profileRoute from './routes/profileRoute.js'
+import notesRoute from './routes/notesRoute.js';
+// import profileRoute from './routes/profileRoute.js'
 import cors from 'cors';
 
 dotenv.config(); 
@@ -20,8 +20,8 @@ app.get('/' , (req , res) => {
     res.send('wellcome to fly high')
 })
 
-// app.use('/api/notes' , notesRoute)
-app.use('/api/auth' , profileRoute)
+app.use('/api/notes' , notesRoute)
+// app.use('/api/auth' , profileRoute)
 
 app.listen(port , ()=>{
     console.log(`Server is runing on port http://localhost:${port}/`);
